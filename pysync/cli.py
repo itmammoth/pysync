7f954d5b79a7d60ac18f6df7722b3eedbdffbfcc
itmammoth <itmammoth@gmail.com>
2019-11-09 12:04:13 +0900

    Correct packeage name to import

--- pysync/cli.py
+++ pysync/cli.py
@@ -1,13 +1,11 @@
-import argparse
-from pysync import pysync
-
-
 def main():
+    import argparse
+    from .core import do_sync
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--settings-file', help='Indicate settings json path')
     args = parser.parse_args()
     settings_file = args.settings_file or 'settings.json'
-    pysync.do_sync(settings_file)
+    do_sync(settings_file)
 
 
 if __name__ == '__main__':
