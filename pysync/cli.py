90d4249203774493eab49791d113081e631860e3
itmammoth <itmammoth@gmail.com>
2019-11-10 12:57:22 +0900

    Check the config file path

--- pysync/cli.py
+++ pysync/cli.py
@@ -2,10 +2,24 @@ def main():
     import argparse
     from .core import do_sync
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--settings-file', help='Indicate settings json path')
+    parser.add_argument('-c', '--config-file', help='Indicate config file path')
     args = parser.parse_args()
-    settings_file = args.settings_file or 'settings.json'
-    do_sync(settings_file)
+    config_path = __resolve_config_file_path(args.config_file, '~/.config/pysync.json', '~/pysync.json')
+    do_sync(config_path)
+
+
+def __resolve_config_file_path(path, *defaults):
+    from pathlib import Path
+    if path:
+        expanded_path = Path(path).expanduser()
+        if expanded_path.exists():
+            return str(expanded_path.resolve())
+        raise Exception('Not found config file `{}`'.format(path))
+    for default_path in defaults:
+        expanded_path = Path(default_path).expanduser()
+        if expanded_path.exists():
+            return str(expanded_path.resolve())
+    raise Exception("Not found config file in [{}]".format(', '.join(defaults)))
 
 
 if __name__ == '__main__':
