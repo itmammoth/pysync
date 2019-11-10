505c621ea5ca5b1e9b2e7cbea0eece4f2b71c5a8
itmammoth <itmammoth@gmail.com>
2019-11-10 12:09:34 +0900

    Remove unnecessary code

--- pysync/core.py
+++ pysync/core.py
@@ -2,7 +2,6 @@ import hjson
 import os
 import subprocess
 from pathlib import Path
-from dictlib import dig_get
 
 
 def do_sync(settings_path):
@@ -12,25 +11,24 @@ def do_sync(settings_path):
     dest_root_path = __get_dest_root_path(settings['destination'])
     dest_root_path.mkdir(parents=True, exist_ok=True)
 
-    filters = dig_get(settings, 'filters', [])
-    global_exclusions = dig_get(settings, 'global_exclusions', [])
+    filters = settings.get('filters', [])
+    global_exclusions = settings.get('global_exclusions', [])
 
     for source in settings['sources']:
-        source_path = Path(source['path'])
         rsync_args = __build_rsync_args(settings)
+
         # filter
         if 'filter' in source:
-            for farg in filters[source['filter']]:
-                rsync_args.append(farg)
+            rsync_args.extend(filters[source['filter']])
         # global exclusions
         for ex in global_exclusions:
-            rsync_args.append('--exclude')
-            rsync_args.append(ex)
+            rsync_args.extend(['--exclude', ex])
         # source exclusions
         if 'exclusions' in source:
             for ex in source['exclusions']:
-                rsync_args.append('--exclude')
-                rsync_args.append(ex)
+                rsync_args.extend(['--exclude', ex])
+
+        source_path = Path(source['path'])
         rsync_args.append(str(source_path.resolve()))
         rsync_args.append(str(dest_root_path.resolve()))
         subprocess.call(rsync_args)
@@ -49,7 +47,7 @@ def __get_dest_root_path(destination_path):
 
 
 def __build_rsync_args(settings):
-    options = dig_get(settings, 'rsync.options')
+    options = settings.get('rsync_options')
     if not options:
         options = ['-a', '-v', '-h']
     return ['rsync'] + options
